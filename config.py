8e87a80bf6ad7f8d740943da8a6955f3008f5aba
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-26 10:50:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://telegra.ph/file/c53d9a7df9dbaa5f4db05.jpg"
-    SUDOERS = filters.user(["6730956183"])
+    MONGO_URL = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
+    START_PIC = "https://files.catbox.moe/ktoo78.jpg"
+    SUDOERS = filters.user(["7078181502"])
