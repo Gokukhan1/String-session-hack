d37b867232b14b3fdcd64f0fae87781b93c4b5bc
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2026-05-11 22:06:54 +0530

    Uncomment TOKEN assignment in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     API_ID = "27079591"
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://files.catbox.moe/ktoo78.jpg"
